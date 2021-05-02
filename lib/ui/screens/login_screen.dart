110b7f176239f6c531b3d022b5bd2f20ac1468e9
Destiny Abellana <d.abellana@arcanys.com>
2021-05-02 17:09:03 +0800

    Added initial login UI

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_lyca/ui/shared/shared.dart';
 
 class LoginScreen extends StatelessWidget {
   static Route route() {
@@ -9,13 +10,46 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Text('Login'),
-            TextField(),
-            TextField(),
-            Divider(height: 1)
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(5.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                decoration: InputDecoration(hintText: 'Email'),
+              ),
+              TextField(
+                decoration: InputDecoration(hintText: 'Password'),
+                obscureText: true,
+              ),
+              ElevatedButton(
+                child: Text('Login'),
+                onPressed: () {},
+              ),
+              DividerWithText(text: 'OR'),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Login with Google'),
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Login with Apple'),
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
