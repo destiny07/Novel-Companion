b25df1866c5c610c1bf46d2c149ee19b50219d1b
Destiny Abellana <d.abellana@arcanys.com>
2021-05-03 11:05:49 +0800

    Added SettingsScreen

--- lib/ui/screens/settings_screen.dart
+++ lib/ui/screens/settings_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_lyca/blocs/authentication/authentication_bloc.dart';
 
 class SettingsScreen extends StatelessWidget {
   static Route route() {
@@ -9,7 +11,14 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text('Settings'),
+        child: ElevatedButton(
+          child: const Text('Logout'),
+          onPressed: () {
+            context
+                .read<AuthenticationBloc>()
+                .add(AuthenticationLogoutRequested());
+          },
+        ),
       ),
     );
   }
