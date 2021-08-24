8699427d7f2c2823945b406590fef2202aaae33f
Destiny Abellana <destiny.abellana7@gmail.com>
2021-08-24 16:00:28 +0800

    Removed dependency on material search

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -26,59 +26,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      // appBar: AppBar(
-      //   actions: [
-      //     IconButton(
-      //       icon: Icon(Icons.settings),
-      //       color: Colors.white,
-      //       onPressed: () {
-      //         Navigator.of(context).push(SettingsScreen.route());
-      //       },
-      //     ),
-      //   ],
-      // ),
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.0),
-          child: BlocProvider(
-            create: (context) {
-              return HomeBloc(
-                dictionaryService: FirebaseDictionaryService(),
-              );
-            },
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                CameraView(cameras: widget.cameras),
-                // Container(
-                //   alignment: Alignment.topCenter,
-                //   child: Padding(
-                //     padding: EdgeInsets.symmetric(horizontal: 8.0),
-                //     child: WordSearchResultCarousel(),
-                //   ),
-                // ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  child: ActionBar(),
-                ),
-              ],
-            ),
+        child: BlocProvider(
+          create: (context) {
+            return HomeBloc(
+              dictionaryService: FirebaseDictionaryService(),
+            );
+          },
+          child: Stack(
+            fit: StackFit.loose,
+            children: [
+              Column(
+                children: [
+                  CameraView(cameras: widget.cameras),
+                  Expanded(child: ActionBar()),
+                ],
+              ),
+              CustomSearchBar(),
+            ],
           ),
         ),
       ),
     );
   }
-
-  
 }
 
-class _SearchBar extends StatelessWidget {
+class CustomSearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<HomeBloc, HomeState>(
@@ -86,59 +64,15 @@ class _SearchBar extends StatelessWidget {
           previous.isShowSearchBar != current.isShowSearchBar,
       builder: (context, state) {
         if (state.isShowSearchBar) {
-          return _searchBar(context);
+          return Container(
+            height: 30,
+            margin: EdgeInsets.fromLTRB(8.0, 64, 8.0, 8.0),
+            child: Padding(padding: EdgeInsets.all(8.0), child: TextField()),
+            color: Colors.white,
+            alignment: Alignment.topCenter,
+          );
         }
         return Container();
-      },
-    );
-  }
-
-  Widget _searchBar(BuildContext context) {
-    final isPortrait =
-        MediaQuery.of(context).orientation == Orientation.portrait;
-
-    return FloatingSearchBar(
-      hint: 'Search...',
-      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-      transitionDuration: const Duration(milliseconds: 800),
-      transitionCurve: Curves.easeInOut,
-      physics: const BouncingScrollPhysics(),
-      axisAlignment: isPortrait ? 0.0 : -1.0,
-      openAxisAlignment: 0.0,
-      width: isPortrait ? 600 : 500,
-      debounceDelay: const Duration(milliseconds: 500),
-      onQueryChanged: (query) {
-        // Call your model, bloc, controller here.
-      },
-      // Specify a custom transition to be used for
-      // animating between opened and closed stated.
-      transition: CircularFloatingSearchBarTransition(),
-      actions: [
-        FloatingSearchBarAction(
-          showIfOpened: false,
-          child: CircularButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {},
-          ),
-        ),
-        FloatingSearchBarAction.searchToClear(
-          showIfClosed: false,
-        ),
-      ],
-      builder: (context, transition) {
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Material(
-            color: Colors.white,
-            elevation: 4.0,
-            child: ListView(
-              shrinkWrap: true,
-              children: Colors.accents.take(2).map((color) {
-                return Container(height: 112, color: color);
-              }).toList(),
-            ),
-          ),
-        );
       },
     );
   }
