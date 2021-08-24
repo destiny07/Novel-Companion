b3758e3d9f95f1800f418f2ec11904cbe7015cc3
Destiny Abellana <destiny.abellana7@gmail.com>
2021-08-24 16:01:19 +0800

    Remove material search import

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:camera/camera.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 import 'package:project_lyca/blocs/blocs.dart';
 import 'package:project_lyca/services/services.dart';
