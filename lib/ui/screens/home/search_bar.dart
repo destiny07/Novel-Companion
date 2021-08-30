f29dfac218a64fe5733d9ed50bb9b67635868c77
Destiny Abellana <destiny.abellana7@gmail.com>
2021-08-30 11:44:17 +0800

    Updated search bar ui

--- lib/ui/screens/home/search_bar.dart
+++ lib/ui/screens/home/search_bar.dart
@@ -4,9 +4,29 @@ class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 30,
-      child: Padding(padding: EdgeInsets.all(8.0), child: TextField()),
-      color: Colors.white,
+      height: 64,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: TextField(
+          style: TextStyle(
+            fontSize: 25.0,
+            color: Colors.blueAccent,
+          ),
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            prefixIcon: Icon(Icons.search),
+            border: InputBorder.none,
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white, width: 32.0),
+              borderRadius: BorderRadius.circular(25.0),
+            ),
+          ),
+        ),
+      ),
       alignment: Alignment.topCenter,
     );
   }
