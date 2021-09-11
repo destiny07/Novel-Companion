871c47910f8093b5561edd743e7c69df9c849abb
Destiny Abellana <destiny.abellana7@gmail.com>
2021-09-11 15:24:24 +0800

    Set uknown screen as blank screen

--- lib/ui/screens/unknown_screen.dart
+++ lib/ui/screens/unknown_screen.dart
@@ -7,8 +7,6 @@ class UnknownScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Invalid Route')),
-    );
+    return Scaffold();
   }
 }
